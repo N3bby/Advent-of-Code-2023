056c073251411c15c6496bac94cc2def461019f5
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2023-12-06 15:33:49 +0100

    Day 6: Part 2 solution (refactor int -> Long + BigDecimal)

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -2,31 +2,32 @@ package day6
 
 import ceilOrIncrement
 import floorOrDecrement
-import kotlin.math.pow
-import kotlin.math.sqrt
+import java.math.MathContext
 
-data class Race(private val raceTime: Int, private val recordDistance: Int) {
-    val chargeTimesBeatingRecord: IntRange get() {
-        val Tr = raceTime.toDouble()
-        val D = recordDistance
+data class Race(private val raceTime: Long, private val recordDistance: Long) {
+    val chargeTimesBeatingRecord: LongRange
+        get() {
+            val Tr = raceTime.toBigDecimal()
+            val D = recordDistance
+            // Tc is charge time
+            //
+            // We want to solve the inequality:
+            // (Tr - Tc) * Tc > D
+            // Tr * Tc - Tc^2 > D
+            // Tc^2 - Tr * Tc - D < 0
+            //
+            // This is a quadractic equation, so we can use (-b ± sqrt(b^2 - 4ac)) / 2a to find zero points
 
-        // Tc is charge time
-        //
-        // We want to solve the inequality:
-        // (Tr - Tc) * Tc > D
-        // Tr * Tc - Tc^2 > D
-        // Tc^2 - Tr * Tc - D < 0
-        //
-        // This is a quadractic equation, so we can use (-b ± sqrt(b^2 - 4ac)) / 2a to find zero points
+            val mathContext = MathContext(10)
 
-        val lowerZeroPoint = (Tr - sqrt(Tr.pow(2) - 4 * D)) / 2.0
-        val upperZeroPoint = (Tr + sqrt(Tr.pow(2) - 4 * D)) / 2.0
+            val lowerZeroPoint = (Tr - (Tr.pow(2) - 4.toBigDecimal() * D.toBigDecimal()).sqrt(mathContext)) / 2.toBigDecimal()
+            val upperZeroPoint = (Tr + (Tr.pow(2) - 4.toBigDecimal() * D.toBigDecimal()).sqrt(mathContext)) / 2.toBigDecimal()
 
-        // If lower- or upperZeroPoint is an integer, then it will match the record for that chargeTime.
-        // These should not be included, as we are only interested in breaking the record.
+            // If lower- or upperZeroPoint is an integer, then it will match the record for that chargeTime.
+            // These should not be included, as we are only interested in breaking the record.
 
-        return lowerZeroPoint.ceilOrIncrement() .. upperZeroPoint.floorOrDecrement()
-    }
+            return lowerZeroPoint.ceilOrIncrement()..upperZeroPoint.floorOrDecrement()
+        }
 }
 
 /**
@@ -40,9 +41,25 @@ fun parseRaces(input: String): List<Race> {
             .split(":")[1]
             .split(" ")
             .filterNot { it.isEmpty() }
-            .map { it.toInt() }
+            .map { it.toLong() }
     }
 
     return parsedLines[0].zip(parsedLines[1])
         .map { (raceTime, recordDistance) -> Race(raceTime, recordDistance) }
+}
+
+/**
+ * Receives (e.g.)
+ * "Time:      7  15   30
+ *  Distance:  9  40  200"
+ */
+fun parseRaceIgnoringKerning(input: String): Race {
+    val (raceTime, recordDistance) = input.lines().map { line ->
+        line
+            .split(":")[1]
+            .replace(" ", "")
+            .toLong()
+    }
+
+    return Race(raceTime, recordDistance)
 }
--- src/test/kotlin/day6/Day6Test.kt
+++ src/test/kotlin/day6/Day6Test.kt
@@ -28,4 +28,23 @@ class Day6Test {
 
         assertThat(result).isEqualTo(211904)
     }
+
+    @Test
+    fun `part 2 - example input`() {
+        val input = """
+            Time:      7  15   30
+            Distance:  9  40  200
+        """.trimIndent()
+
+        val race = parseRaceIgnoringKerning(input)
+
+        assertThat(race.chargeTimesBeatingRecord.count()).isEqualTo(71503)
+    }
+
+    @Test
+    fun `part 2 - puzzle input`() {
+        val race = parseRaceIgnoringKerning(readInput(6))
+
+        assertThat(race.chargeTimesBeatingRecord.count()).isEqualTo(0)
+    }
 }
