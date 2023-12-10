1b7eaaf5950281f81066708f8606b038b59a39dc
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2023-12-10 21:43:19 +0100

    Tinker with the Eta helper class

--- src/main/kotlin/util/Eta.kt
+++ src/main/kotlin/util/Eta.kt
@@ -15,25 +15,28 @@ class Eta(private val totalUnitsToProcess: Long, private val delayBetweenUpdates
         printEtaIfNeeded()
     }
 
+    // TODO Check if this is correct. It doesn't seem to product accurate estimates
     private fun printEtaIfNeeded() {
         val now = System.currentTimeMillis()
-        synchronized(this) {
-            if (now - lastPrintTimestamp >= delayBetweenUpdatesInMs) {
+        if (now - lastPrintTimestamp >= delayBetweenUpdatesInMs) {
+            synchronized(this) {
+                if (now - lastPrintTimestamp >= delayBetweenUpdatesInMs) {
 
-                val percentage = processedUnits.get() / totalUnitsToProcess.toDouble()
-                val formattedPercentage = (round(percentage * 100_000) / 1000).toString() + " %"
+                    val percentage = processedUnits.get() / totalUnitsToProcess.toDouble()
+                    val formattedPercentage = (round(percentage * 100_000) / 1000).toString() + " %"
 
-                val estimatedTotalElapsedTime = (now - startedAtTimestamp) / percentage
-                val estimatedFinishTimestamp = startedAtTimestamp + estimatedTotalElapsedTime
-                val etaTotalSeconds = round((estimatedFinishTimestamp - now) / 1000)
-                val etaMinutes = floor(etaTotalSeconds / 60).toInt()
-                val etaRemainderSeconds = (etaTotalSeconds % 60).toInt()
+                    val estimatedTotalElapsedTime = (now - startedAtTimestamp) / percentage
+                    val estimatedFinishTimestamp = startedAtTimestamp + estimatedTotalElapsedTime
+                    val etaTotalSeconds = round((estimatedFinishTimestamp - now) / 1000)
+                    val etaMinutes = floor(etaTotalSeconds / 60).toInt()
+                    val etaRemainderSeconds = (etaTotalSeconds % 60).toInt()
 
-                val formattedEta = "[ETA: ${etaMinutes}m ${etaRemainderSeconds}s]"
+                    val formattedEta = "[ETA: ${etaMinutes}m ${etaRemainderSeconds}s]"
 
-                println("Progress: $formattedPercentage $formattedEta")
+                    println("Progress: $formattedPercentage $formattedEta")
 
-                lastPrintTimestamp = now
+                    lastPrintTimestamp = now
+                }
             }
         }
     }
