582935c9c339445f4f4ebdc2c54faefcbf9a1de2
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2023-12-11 22:30:53 +0100

    Day 11: Better variable naming

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -6,27 +6,27 @@ import ext.getAllUniqueCombinations
 import util.Line
 import util.Position
 
-data class Universe(private val galaxies: List<Galaxy>, private val age: Int = 1) {
+data class Universe(private val galaxies: List<Galaxy>, private val ageOfUniverse: Int = 1) {
 
     val sumOfDistancesBetweenGalaxies: Long
         get() = galaxies
             .getAllUniqueCombinations()
             .map { (galaxy1, galaxy2) -> Line(galaxy1.position, galaxy2.position) }
             .sumOf { line ->
-                val expandedUnitsCrossed = expandedSpace.count { it.isCrossedBy(line) }
-                line.manhattanDistance.toLong() - expandedUnitsCrossed + expandedUnitsCrossed * age
+                val expandedSpacesCrossed = expandedSpaces.count { it.isCrossedBy(line) }
+                line.manhattanDistance.toLong() - expandedSpacesCrossed + expandedSpacesCrossed * ageOfUniverse
             }
 
-    private val expandedSpace: List<ExpandedSpace> by lazy {
-        emptyRowLocations.map { ExpandedSpace(it, HORIZONTAL) } +
-        emptyColumnLocations.map { ExpandedSpace(it, VERTICAL) }
+    private val expandedSpaces: List<ExpandedSpace> by lazy {
+        emptyRowPositions.map { ExpandedSpace(it, HORIZONTAL) } +
+        emptyColumnPositions.map { ExpandedSpace(it, VERTICAL) }
     }
 
-    private val emptyRowLocations: List<Int> by lazy {
+    private val emptyRowPositions: List<Int> by lazy {
         (minY..maxY).filter { y -> galaxies.none { it.position.y == y } }
     }
 
-    private val emptyColumnLocations: List<Int> by lazy {
+    private val emptyColumnPositions: List<Int> by lazy {
         (minX..maxX).filter { x -> galaxies.none { it.position.x == x } }
     }
 
